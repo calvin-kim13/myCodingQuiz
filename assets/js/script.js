ca8c2ca8d50fa35eea916f9973d52f5ce9b72d95
Calvin Kim <calvinkim1230@gmail.com>
2022-01-13 04:23:35 -0800

    updated HTML and CSS and added pseudocode for JS

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Start Quiz Button
+
+// Start Timer when start button is pressed
+
+// Questions Presented
+
+// Right Answers => text content as correct and next question
+
+// Wrong Answers => -10 seconds from timer, text as wrong and next question
+
+// If all questions are answered or timer reachers 0, game is over
+
+// Input initials, submit it, and save it to local storage and display it in leaderboards
+
+// Back to homepage (create functions within functions)
+
+
+
+
+
+
+     
