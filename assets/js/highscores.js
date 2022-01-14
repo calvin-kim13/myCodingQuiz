e38fd8882a233c2ab54f31440396154490819150
Calvin Kim <calvinkim1230@gmail.com>
2022-01-14 00:05:35 -0800

    added multiple html, css, and js files to create separate files for the game page, end page, and leaderboard page, and homepage then linked them together

---
+++ assets/js/highscores.js
@@ -0,0 +1,7 @@
+const highScoresList = document.querySelector('#highScoresList');
+const highScoresList = JSON.parse(localStorage.getItem('highScores')) || [];
+
+highScoresList.innerHTML = 
+highScores.map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`
+}).join('');
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,121 @@
-// Start Quiz Button
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
 
-// Start Timer when start button is pressed
+let currentQuestion = {}
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
 
-// Questions Presented
+let questions = [
+    {
+        question: 'Which of the following is NOT an example of a data type:',
+        choice1: 'numbers',
+        choice2: 'booleans',
+        choice3: 'strings',
+        choice4: 'functions',
+        answer: 4,
+    },
+    {
+        question: 'What will the code, console.log(typeof "2"), display on the console?',
+        choice1: 'number',
+        choice2: 'string',
+        choice3: 'true',
+        choice4: 'false',
+        answer: 2,
+    },
+    {
+        question: 'What is the correct way to set a value to a variable?',
+        choice1: 'var element = 2',
+        choice2: 'varialbe element = 2',
+        choice3: 'v element = 2',
+        choice4: 'var element: 2',
+        answer: 1,
+    },
+    {
+        question: 'What is the correct way to call a function?',
+        choice1: 'write out function',
+        choice2: 'it automatically calls it',
+        choice3: 'write out the function name with () at the end',
+        choice4: 'write out the function name',
+        answer: 3,
+    },
+    {
+        question: 'Which of the following is the correct way to get an element from the HTML page:',
+        choice1: 'document.getElementById()',
+        choice2: '.getElementById',
+        choice3: 'doc.getElementById()',
+        choice4: 'getId()',
+        answer: 1,
+    }
+]
 
-// Right Answers => text content as correct and next question
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 5;
 
-// Wrong Answers => -10 seconds from timer, text as wrong and next question
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestions();
+}
 
-// If all questions are answered or timer reachers 0, game is over
+getNewQuestions = () => {
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
 
-// Input initials, submit it, and save it to local storage and display it in leaderboards
+        return window.location.assign('/end.html');
+    }
 
-// Back to homepage (create functions within functions)
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`;
 
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionsIndex];
+    question.innerText = currentQuestion.question;
 
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    })
 
+    availableQuestions.splice(questionsIndex, 1);
 
+    acceptingAnswers = true;
+}
 
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return;
 
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        if (classToApply === 'correct') {
+            incrementScore(SCORE_POINTS);
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestions();
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num;
+    scoreText.innerText = score;
+}
+
+startGame();
      
